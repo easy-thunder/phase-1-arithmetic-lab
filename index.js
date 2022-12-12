5a5adb23c2ebad3163dba202df37540e11a22c95
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-12 09:33:26 -0700

    commit tree 7d4cf2317f5dbbf1a0cb4cb764f10d7762c70f44

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+
+let num1=31;
+let num2=2;
+let multiply = num1*num2;
+
+
+let random = Math.trunc(Math.random()*10)+1;
+let num3 = 53;
+let num4 = 7;
+let  mod= num3%num4;
+
+let max = Math.max(2,4,5,20);
+
+
